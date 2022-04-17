835e4c16e0a12683447fa39a4ce0e7d39b6e141a
Everton Rocha <irocha10@hotmail.com>
2022-04-17 07:17:46 -0300

    Tabbed component

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 ///////////////////////////////////////
 // Modal window
 
@@ -33,16 +36,16 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//Scroll
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+//////////////////////////////////////
+////////////
 
+//Page Navigation
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   //console.log(s1coords);
 
   //Current button
-  console.log(e.target.getBoundingClientRect());
+  //console.log(e.target.getBoundingClientRect());
 
   //Scrolling (Old Skull)
   // window.scrollTo({
@@ -55,4 +58,59 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({
     behavior: 'smooth',
   });
+});
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     //Get attr
+//     const id = this.getAttribute('href');
+
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //console.log(e.target);
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
+// document.querySelector('h1').closest('.header').style.background =
+//   'var(--color-secondary)';
+
+////////////////////////////////////////
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//Delegate event handler to parent element
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard
+  if (!clicked) return;
+
+  //Remove and add classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
